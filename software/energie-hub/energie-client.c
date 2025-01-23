8f34ae5fd7b52a2eb33b3ac2662971a8635ca87a
Manuel Bouyer <bouyer@antioche.eu.org>
2025-01-23 14:22:13 +0100

    Add a client/server tool to get data from, and send commands to the energy board. The hub connects to the serial port,  get commands from client for the boards and relays messages from board to clients

---
+++ software/energie-hub/energie-client.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2025 Manuel Bouyer
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <string.h>
+#include <err.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/.e_hub"
+
+#define LINESZ 128
+
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: %s [command] ...\n", getprogname());
+	exit(1);
+}
+
+int
+main(int argc, char * const argv[])
+{
+	int ch;
+	int sockfd;
+	FILE *sock_f;
+	struct sockaddr_un saddr;
+	int i;
+
+	while ((ch = getopt(argc, argv, "h")) != -1) {
+		switch(ch) {
+		case 'h':
+		case '?':
+		default:
+			usage();
+		}
+	}
+
+	if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+		err(EXIT_FAILURE, "socket");
+	}
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sun_family = AF_UNIX;
+	strncpy(saddr.sun_path, SOCKET_PATH, sizeof(saddr.sun_path) - 1);
+
+	if (connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
+		err(EXIT_FAILURE, "connect %s", SOCKET_PATH);
+	}
+
+	sock_f = fdopen(sockfd, "w+");
+	if (sock_f == NULL)
+		err(1, "fopen %s", SOCKET_PATH);
+
+	/* send strings from command line */
+	for (i = 1; i < argc; i++) {
+		if (fprintf(sock_f, "%s\n", argv[i]) < 0 ||
+		    fflush(sock_f) < 0) {
+			err(1, "send failed");
+		}
+	}
+
+	while (1) {
+		struct pollfd fds[2];
+		static char linebuf[LINESZ];
+
+		fds[0].fd = STDIN_FILENO;
+		fds[0].events = POLLRDNORM;
+		fds[1].fd = sockfd;
+		fds[1].events = POLLRDNORM;
+
+		if (poll(fds, 2, -1) < 0) {
+			err(1, "poll");
+		}
+		if (fds[0].revents & POLLRDNORM) {
+			if (fgets(linebuf, LINESZ, stdin) == NULL) {
+				fclose(sock_f);
+				exit(0);
+			}
+			if (fprintf(sock_f, "%s", linebuf) < 0 ||
+			    fflush(sock_f) < 0) {
+				err(1, "can't write to socket");
+			}
+		}
+		if (fds[1].revents & POLLRDNORM) {
+			if (fgets(linebuf, LINESZ, sock_f) == NULL) {
+				errx(1, "can't read from socket");
+			}
+			if (fprintf(stdout, "%s", linebuf) < 0 ||
+			    fflush(stdout) < 0) {
+				err(1, "write to stdout");
+			}
+		}
+	}
+}
---
+++ software/energie-hub/energie-hub.c
@@ -0,0 +1,291 @@
+/*
+ * Copyright (c) 2025 Manuel Bouyer
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <string.h>
+#include <err.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <termios.h>
+
+#define SOCKET_PATH "/tmp/.e_hub"
+
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: %s <linky tty>\n", getprogname());
+	exit(1);
+}
+
+static int opt_f = 0;
+#define MAX_CLIENTS 128
+#define LINESZ 80
+
+static struct clients_fds {
+	int fd;
+	FILE *f;
+} clients_fds[MAX_CLIENTS] = {0};
+static int nclients = 0;
+
+static int linky;
+static FILE *linky_f;
+
+static void
+linky_write(char *buf)
+{
+	int s = strlen(buf);
+	unsigned int sum = 0;
+	int i;
+
+	/* strip newline */
+	if (buf[s - 1] == '\n') {
+		buf[s - 1]  = '\0';
+		s--;
+	}
+	printf("write to linky %d %s\n", s, buf);
+	/* strip 8th bit, compute checksum */
+	for (i = 0; i < s; i++) {
+		buf[i] = buf[i] & 0x7f;
+		if (buf[i] < 0x20) {
+			printf("write ignore %d %d\n", i, buf[i]);
+			
+			return; /* ignore bad chars */
+		}
+		sum += buf[i];
+	}
+	if (fprintf(linky_f, buf) < 0)
+		err(1, "linky write");
+
+	sum = (sum & 0x3f) + 0x20;
+	if (fprintf(linky_f, " %c\r\n", sum) < 0 || fflush(linky_f) < 0)
+		err(1, "linky write");
+}
+
+static void
+clients_write(char *buf)
+{
+	int s = strlen(buf);
+	unsigned int sum = 0;
+	int i;
+
+	/* verify checksum */
+	for (i = 0; i < s - 2; i++) {
+		buf[i] = buf[i] & 0x7f;
+		sum += buf[i];
+	}
+	sum = (sum & 0x3f) + 0x20;
+	if (s < 2 || buf[s - 2] != ' ' || buf[s - 1] != sum) {
+		warnx("linky sum mismatch: %s %d %d != %d", buf, s, sum, buf[s - 1]);
+		return;
+	}
+	/* strip checksum */
+	buf[s - 2] = '\0';
+	/* send to clients */
+	for (i = 0; i < nclients; i++) {
+		if (clients_fds[i].f == NULL)
+			continue;
+		printf("send to client %d\n", i);
+		if (fprintf(clients_fds[i].f, "%s\n", buf) < 0 ||
+		    fflush(clients_fds[i].f) < 0) {
+			printf("close client %d\n", i);
+			fclose(clients_fds[i].f);
+			clients_fds[i].f = NULL;
+			close(clients_fds[i].fd);
+		}
+	}
+}
+
+int
+main(int argc, char * const argv[])
+{
+	int ch;
+	extern int optind;
+	int main_socket;
+	struct sockaddr_un saddr;
+	struct termios cntrl;
+	int i;
+	static char linebuf[LINESZ];
+	static char linkybuf[LINESZ];
+	int linkybufi = 0;
+	struct pollfd fds[MAX_CLIENTS + 2];
+	int nfds;
+
+	while ((ch = getopt(argc, argv, "f")) != -1) {
+		switch(ch) {
+		case 'f':
+			opt_f++;
+			break;
+		case '?':
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+	if (argc != 1)
+		usage();
+
+	unlink(SOCKET_PATH);
+	/* need to open with O_NONBLOCK, util we set CLOCAL */
+	linky = open(argv[0], O_RDWR | O_NOCTTY | O_NONBLOCK);
+	if (linky < 0)
+		err(1, "open %s", argv[0]);
+
+	tcgetattr(linky, &cntrl);
+	cfsetospeed(&cntrl, 57600);
+	cfsetispeed(&cntrl, 57600);
+	cntrl.c_cflag &= ~(CSIZE|PARENB);
+	cntrl.c_cflag |= CS8;
+	cntrl.c_cflag |= CLOCAL;
+	cntrl.c_iflag &= ~(ISTRIP|ICRNL);
+	cntrl.c_oflag &= ~OPOST;
+	cntrl.c_lflag &= ~(ICANON|ISIG|IEXTEN|ECHO);
+	cntrl.c_cc[VMIN] = 1;
+	cntrl.c_cc[VTIME] = 0;
+	if (tcsetattr(linky, TCSAFLUSH, &cntrl) != 0) {
+		err(1, "tcsetattr %s", argv[0]);
+	}
+	if ((i = fcntl(linky, F_GETFL, 0)) < 0)  {
+		err(1, "F_GETFL %s", argv[0]);
+	}
+	if (fcntl(linky, F_SETFL, i & ~O_NONBLOCK) < 0) {
+		err(1, "F_SETFL %s", argv[0]);
+	}
+
+	linky_f = fdopen(linky, "w+");
+	if (linky_f == NULL)
+		err(1, "fopen %s", argv[1]);
+
+	main_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (main_socket < 0)
+		err(1, "create %s", SOCKET_PATH);
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sun_family = AF_UNIX;
+	strncpy(saddr.sun_path, SOCKET_PATH, sizeof(saddr.sun_path) - 1);
+
+	if (bind(main_socket, (struct sockaddr *)&saddr, sizeof(saddr)) != 0)
+		err(1, "bind %s",  SOCKET_PATH);
+
+	if (listen(main_socket, 10) != 0)
+		err(1, "listen %s",  SOCKET_PATH);
+
+#if 0
+	if (opt_f == 0) {
+		if (daemon(0, 0) != 0)
+			err(1, "daemon()");
+	}
+#endif
+
+	printf("ready\n");
+	while (1) {
+		for (i = 0; i < nclients; i++) {
+			fds[i].fd = clients_fds[i].fd;
+			fds[i].events = POLLRDNORM;
+		}
+		fds[i].fd = linky;
+		fds[i].events = POLLRDNORM;
+		i++;
+		fds[i].fd = main_socket;
+		fds[i].events = POLLRDNORM;
+		nfds = i + 1;
+
+		if (poll(fds, nfds, -1) < 0) {
+			err(1, "poll");
+		}
+		for (i = 0; i < nclients; i++) {
+			if (fds[i].revents & POLLRDNORM) {
+				printf("read from client %d\n", i);
+				if (fgets(linebuf, LINESZ, clients_fds[i].f)
+				    == NULL) {
+					printf("close client %d\n", i);
+					fclose(clients_fds[i].f);
+					clients_fds[i].f = NULL;
+					close(clients_fds[i].fd);
+				} else {
+					linky_write(linebuf);
+				}
+			}
+		}
+		if (fds[i].revents & POLLRDNORM) {
+			switch(read(linky, &linkybuf[linkybufi], 1)) {
+			case 0:
+				/* ignore short read */
+				break;
+			case -1:
+				errx(1, "can't read linky");
+			default:
+				if (linkybuf[linkybufi] == '\r') {
+					/* complete line */
+					linkybuf[linkybufi] = '\0';
+					clients_write(linkybuf);
+					linkybufi = 0;
+				} else if (linkybuf[linkybufi] < 0x20) {
+					/* ignore */
+				} else if (linkybufi < LINESZ - 2) {
+					linkybufi++;
+				}
+			}
+		}
+		i++;
+		if (fds[i].revents & POLLRDNORM) {
+			int new_s;
+			socklen_t socksize = sizeof(saddr);
+			printf("accept new client\n");
+			new_s = accept4(main_socket, (struct sockaddr *)&saddr,
+			    &socksize, SOCK_NOSIGPIPE);
+			if (nclients < MAX_CLIENTS) {
+				clients_fds[nclients].fd = new_s;
+				clients_fds[nclients].f =
+				    fdopen(new_s, "w+");
+				if (clients_fds[nclients].f == NULL) {
+					warn("fdopen new client");
+					close(new_s);
+				} else {
+					printf("added client %d\n", nclients);
+					nclients++;
+				}
+			} else {
+				close(new_s);
+			}
+		}
+		/* cleanup closed clients, compat array */
+		for (i = 0 ; i < nclients; i++) {
+			if (clients_fds[i].f != NULL)
+				continue;
+			printf("cleanup client %d\n", i);
+			memmove(&clients_fds[i], &clients_fds[i+1],
+			    sizeof(struct clients_fds) * (nclients - (i + 1)));
+			nclients--;
+		}
+			    
+	}
+}
